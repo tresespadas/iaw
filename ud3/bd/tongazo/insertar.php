1150a3fb1587bc13522171f42abb955e9bbf9535
tresespadas <alvaro8038c@gmail.com>
2025-11-25 06:15:48 +0100

    ej tongazo

--- ud3/bd/tongazo/insertar.php
+++ ud3/bd/tongazo/insertar.php
@@ -6,6 +6,7 @@
     <title>Insertar vehículo - Tongazo Seguro</title>
   </head>
   <body>
+  <h2>Insertar vehículo</h2>
   <?php
   require 'funciones.php';
   $pdo = conecta("localhost","tongazo","root");
@@ -13,6 +14,29 @@
   {
     echo "Ha ocurrido un error con la conexión";
     exit;
+  }
+  ?>
+  <form action="insertar.php" method="post">
+    <label for="matricula">Matrícula</label>
+    <input type="text" id="matricula" name="matricula" required pattern="[0-9]{4}[A-Z]{3}"><br>
+    <label for="marca">Marca</label>
+    <input type="text" id="marca" name="marca" required><br>
+    <label for="modelo">Modelo</label>
+    <input type="text" id="modelo" name="modelo" required><br>
+    <label for="kms">Kms</label>
+    <input type="number" id="kms" name="kms" required step="0.1" min="0"><br>
+    <label for="precio">Precio</label>
+    <input type="number" id="precio" name="precio" required step="0.01" min="0"><br>
+    <input type="submit" value="Guardar en la BD">
+  </form>
+  <?php
+  try
+  {
+    $pdo->query("INSERT INTO vehiculos (matricula,marca,modelo,kms,precio) VALUES ('$matricula','$marca','$modelo','$kms','$precio')");
+  }
+  catch (PDOException $e)
+  {
+    echo "La conexión ha fallado por ".$e->getMessage();
   }
   ?>
   </body>
