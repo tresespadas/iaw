73d8a5e7be3863886254d776005d70829440df12
tresespadas <alvaro8038c@gmail.com>
2026-01-05 06:42:37 +0100

    error al buscar pelicula

--- ud3/bd/repaso-recuperacion2/buscar.php
+++ ud3/bd/repaso-recuperacion2/buscar.php
@@ -16,7 +16,7 @@ require 'funciones.php';
         echo "<fieldset>";
         echo "<legend>Busca una película o serie de TV</legend>";
         echo "<label for='nombre'>Nombre </label>";
-        echo "<input type='text' name='nombre' id='nombre'><br>";
+        echo "<input type='text' name='nombre' id='nombre' required><br>";
         echo "<label for='tipo'>Tipo </label>";
         echo "<select name='tipo'>";
         $tipos = leer_tipos("lista_tipos.txt");
@@ -29,7 +29,57 @@ require 'funciones.php';
         echo "</fieldset>";
         echo "<input type='submit' name='buscar' id='buscar' value='Buscar'>";
         echo "</form>";
+
+        if (isset($_POST['buscar']))
+        {
+            $nombre = $_POST['nombre'];
+            $tipo = $_POST['tipo'];
+
+            $pdo = conecta('netflix');
+
+            if ($tipo == 'película')
+            {
+                $busqueda = $pdo->query("SELECT titulo, fecha_estreno, duracion FROM peliculas WHERE titulo LIKE '%$nombre%'");
+
+                echo "<table>";
+                echo "<tr>";
+                echo "<th>Título</th>";
+                echo "<th>Fecha de estreno</th>";
+                echo "<th>Duración</th>";
+                echo "</tr>";
+                while ($fila = $busqueda->fetch())
+                {
+                    echo "<tr>";
+                    echo "<td>$fila[titulo]</td>";
+                    echo "<td>$fila[fecha_estreno]</td>";
+                    echo "<td>$fila[duracion]</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+            else
+            {
+                $busqueda = $pdo->query("SELECT series.titulo, temporadas.fecha_estreno, temporada FROM series LEFT JOIN temporadas ON series.id=temporadas.id_serie WHERE series.titulo LIKE '%$nombre%'");
+
+                echo "<table>";
+                echo "<tr>";
+                echo "<th>Título</th>";
+                echo "<th>Fecha de estreno</th>";
+                echo "<th>Temporada</th>";
+                echo "</tr>";
+                while ($fila = $busqueda->fetch())
+                {
+                    echo "<tr>";
+                    echo "<td>$fila[titulo]</td>";
+                    echo "<td>$fila[fecha_estreno]</td>";
+                    echo "<td>$fila[temporada]</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+        }
     ?>
+    <a href="index.php">Volver al inicio</a>
     
 </body>
 </html>
